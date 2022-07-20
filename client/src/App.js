d9e275ac7c7d78280d700fb2b28b7568ccdc4c63
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-07-20 17:23:56 +0530

    day-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import { LabTest } from "./Pages/LabTest";
-import Mainroutes from "./Routes/Mainroutes";
+import Healthcare from "./Healthcare Page/Healthcare";
+import { SliderData } from "./Healthcare Page/SliderData";
+// import { LabTest } from "./Pages/LabTest";
+// import Mainroutes from "./Routes/Mainroutes";
 
 
 function App() {
   return (
     <div >
-     <Mainroutes/>
-      <LabTest/>
+     {/* <Mainroutes/>
+      <LabTest/> */}
+      <Healthcare slides={SliderData}/>
     </div>
   );
 }
