4069b1c51e162e331fd12630cecc387dbcf62783
Monica Priya <100186167+Monica1310@users.noreply.github.com>
2022-07-20 11:59:35 +0530

    monica-branch

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-<<<<<<< HEAD
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-=======
-import { ChakraProvider } from '@chakra-ui/react'
-import {BrowserRouter} from "react-router-dom"
->>>>>>> 92ba2e2c6bdf6c1ad74feca2f9785204931357bb
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
