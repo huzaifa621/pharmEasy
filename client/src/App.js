d1dcc36a6510c0e7a0bdb06758bb06e32c19e461
Nandu Dumare <dumarenandu70@gmail.com>
2022-07-21 21:55:15 +0530

    navbar-thing

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-
 import Healthcare from "./Healthcare Page/Healthcare";
 import Product from "./Healthcare Page/Product";
 import { SliderData } from "./Healthcare Page/SliderData";
 
 import { Footer } from "./Components/Footer";
 import { AppDownload } from "./Pages/home/AppDownload";
-import {FullScreenSlider} from "./Pages/home/FullScreenSlider";
-import { Navbar } from "./Components/Navbar";
-
+import { FullScreenSlider } from "./Pages/home/FullScreenSlider";
+import { Navbar } from "./Components/navbar/Navbar";
 
 function App() {
   return (
     <div>
-     {/* <Mainroutes/>
+      {/* <Mainroutes/>
       <LabTest/> */}
-      <Healthcare slides={SliderData}/>
-      <Product/>
-
+      {/* <Healthcare slides={SliderData}/> */}
+      {/* <Product/> */}
+      <Navbar />
     </div>
   );
 }
