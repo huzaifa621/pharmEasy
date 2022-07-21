55df91162b943e570e632394953ce1fc77f808d7
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-07-21 22:03:23 +0530

    all

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 
-import Healthcare from "./Healthcare Page/Healthcare";
+// import Healthcare from "./Healthcare Page/Healthcare";
 import Product from "./Healthcare Page/Product";
-import { SliderData } from "./Healthcare Page/SliderData";
+// import { SliderData } from "./Healthcare Page/SliderData";
 
 import { Footer } from "./Components/Footer";
-import { AppDownload } from "./Pages/home/AppDownload";
-import {FullScreenSlider} from "./Pages/home/FullScreenSlider";
-import { Navbar } from "./Components/Navbar";
+// import { AppDownload } from "./Pages/home/AppDownload";
+// import {FullScreenSlider} from "./Pages/home/FullScreenSlider";
+// import { Navbar } from "./Components/Navbar";
 
 
 function App() {
   return (
     <div>
      {/* <Mainroutes/>
-      <LabTest/> */}
-      <Healthcare slides={SliderData}/>
+      <LabTest/>
+      <Navbar/>
+      <Healthcare slides={SliderData}/> */}
       <Product/>
+      <Footer/>
 
     </div>
   );
