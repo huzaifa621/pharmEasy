3aacd0d99adb45b595a29562d6872f5d737c92b8
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-07-22 13:51:41 +0530

    data

--- client/src/Routes/Mainroutes.jsx
+++ client/src/Routes/Mainroutes.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Desc from "../Pages/Desc";
 import LabDetails from "../Pages/Labdetails/LabDetails";
+import { LabTest } from "../Pages/LabTest";
 import OrderMedicines from "../Pages/OrderMedicines";
 import { Rtcprtest } from "../Pages/RtpcrTest/Rtcprtest";
 
 const Mainroutes = () => {
   return (
     <div>
-      <Rtcprtest/>
       <Routes>
         <Route path="productdetails" element={<Desc />} />
         <Route path="med" element={<OrderMedicines />} />
