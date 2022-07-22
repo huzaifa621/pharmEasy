3f84d9fb679d00e2fca85451a43ce2d4eb03dd83
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-07-22 13:47:47 +0530

    rtpcr

--- client/src/Routes/Mainroutes.jsx
+++ client/src/Routes/Mainroutes.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Desc from "../Pages/Desc";
 import LabDetails from "../Pages/Labdetails/LabDetails";
 import OrderMedicines from "../Pages/OrderMedicines";
+import { Rtcprtest } from "../Pages/RtpcrTest/Rtcprtest";
 
 const Mainroutes = () => {
   return (
     <div>
+      <Rtcprtest/>
       <Routes>
         <Route path="productdetails" element={<Desc />} />
         <Route path="med" element={<OrderMedicines />} />
